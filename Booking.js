007203919d269b7f93dd9447b983edd968f8269a
NightMare489 <56297925+NightMare489@users.noreply.github.com>
2023-11-18 10:28:49 +0200

    hamburger

---
+++ Booking.js
@@ -0,0 +1,19 @@
+function hambruger(){
+     var sidebar = document.getElementById("sideBar");
+     var hambruger = document.getElementById("hambruger");
+
+     if(sidebar.classList.contains("notactive")){
+
+        sidebar.classList.remove("notactive");
+        sidebar.classList.add("active");
+        hambruger.classList.add("active");
+
+     }else{
+
+        sidebar.classList.remove("active");
+        sidebar.classList.add("notactive");
+        hambruger.classList.remove("active");
+
+     }
+    
+}
