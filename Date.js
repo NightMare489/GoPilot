5df6b96da39b3719eace6f8e3b29e42af14eac26
Amr Ashraf <110328592+Timooncroco@users.noreply.github.com>
2024-01-01 21:13:54 +0200

    nav php

--- Date.js
+++ Date.js
@@ -1,23 +1,3 @@
-function hambruger(){
-    var sidebar = document.getElementById("sideBar");
-    var hambruger = document.getElementById("hambruger");
-
-    if(sidebar.classList.contains("notactive")){
-
-       sidebar.classList.remove("notactive");
-       sidebar.classList.add("active");
-       hambruger.classList.add("active");
-
-    }else{
-
-       sidebar.classList.remove("active");
-       sidebar.classList.add("notactive");
-       hambruger.classList.remove("active");
-
-    }
-   
-
-}
 
 const daysContainer = document.getElementById("daysContainer");
 const prevBtn = document.getElementById("prevBtn");
--- Date.php
+++ Date.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Date</title>
     <link rel="stylesheet" href="./Date.css?v=<?php echo time(); ?>">
@@ -12,39 +12,9 @@
 </head>
 <body>
 <!-- app bar -->
-     <div id="menu" class="card">
-      <div id="logo">
-        <a href="index.php">
-          <img src="icons/navlogo.png" alt="logo" id="logoimg" />
-        </a>
-      </div>
-      <div class="rightblock">
-        <div class="menuItemDiv">
-            <a href="" class="menuItem" id="haha">Dashboard </a>
-          </div>
-        <div class="menuItemDiv" >
-          <a href="" class="menuItem">Amr Asd</a>
-        </div>
-        <div class="menuItem menuItemDiv" id="coma">|
-        </div>
-        <div class="menuItemDiv">
-          <a href="" class="menuItem">Log Out</a>
-        </div>
-        <div class="hambruger">
-          <span id="hambruger" class="Hambrger" onclick="hambruger()">☰</span>
-        </div>
-      </div>
-      </div>
-
-      <div id="sideBar" class="sideBar notactive">
-        <div class="SideBarItems">
-          <a  class="SideBarItemsDiv" href="" >
-            Dashboard 
-        </a>
-            <a  class="SideBarItemsDiv"  href="">Amr Asd</a>
-            <a  class="SideBarItemsDiv" href="">Log Out</a>
-        </div>
-      </div>
+<?php
+include"nav.php";
+?>
 <!-- end of app bar -->
 
 <div class="mnn">
