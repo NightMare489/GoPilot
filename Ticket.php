6e69ca22694d4655fa94bfb85c090c30dcaa8e10
Omar Farouk <ofarouk837@gmail.com>
2024-01-03 03:12:31 +0200

    Ticket responsive

--- Ticket.php
+++ Ticket.php
@@ -174,7 +174,7 @@
     <div id="container">
     <!-- level 1 -->
     <div style="background-color: #003F60;" id="level1">
-      <img src="icons/logo.png" alt="logo" style="width: 213px ; height: 100px ; margin-left: 20px ">
+      <img src="icons/logo.png" alt="logo" >
 
 
       <div id="Llev1">
@@ -208,14 +208,14 @@
     <!-- level 3 -->
     <div id="level3">
     <div class="conlev3" style="width: 50%; height:83%;background-color: #DFF6FF;">
-        <p class="label" style="margin-top: 20px;">Date</p>
+        <p class="label" >Date</p>
         <p class="value" style="margin-bottom: 30px;">
         <?php
             echo Date("d/m/Y",strtotime($meetCaptinDateTime));
 
         ?></p>
 
-        <p class="label" style="margin-top: 30px;">Meet Captain at</p>
+        <p class="label" >Meet Captain at</p>
         <p class="value">
         <?php
             echo Date("h:i A",strtotime($meetCaptinDateTime));
@@ -224,18 +224,18 @@
     </div>
 
     <div class="conlev3" style="width: 50%; height:83%">
-        <p class="label" style="margin-top: 20px;">Number of Persons </p>
+        <p class="label">Number of Persons </p>
         <p class="value" style="margin-bottom: 30px;"><?php echo $numPersons?></p>
 
-        <p class="label" style="margin-top: 30px;">Number of Bags</p>
+        <p class="label">Number of Bags</p>
         <p class="value"><?php echo $numBags?></p>
     </div>
     
      <div class="conlev3" style="background-color: #DFF6FF; width:50%;height:83%">
-     <p class="label" style="margin-top: 20px;">Expected Time</p>
+     <p class="label">Expected Time</p>
         <p class="value" style="margin-bottom: 30px;"><?php echo $durationTxt ?></p>
 
-        <p class="label" style="margin-top: 30px;">Price</p>
+        <p class="label">Price</p>
         <p class="value"><?php echo $price ?> L.E</p>
      </div>
     </div>
